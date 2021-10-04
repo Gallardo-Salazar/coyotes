aeb685d054cd970ed5f994237862da4f1a7f8b8c
unknown <pe_gasa@hotmail.com>
2021-10-04 04:19:24 -0500

    Initial Commit

---
+++ pointclouds/index/cloud.js
@@ -0,0 +1,67 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 5634785,
+    "boundingBox": {
+        "lx": 490112.36,
+        "ly": 2641518.38,
+        "lz": 2612.4,
+        "ux": 490466.58,
+        "uy": 2641872.6,
+        "uz": 2966.6200000000304
+    },
+    "tightBoundingBox": {
+        "lx": 490112.36,
+        "ly": 2641518.38,
+        "lz": 2612.4,
+        "ux": 490466.58,
+        "uy": 2641759.92,
+        "uz": 2640.39
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "classification",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 3.0676352977752687,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
